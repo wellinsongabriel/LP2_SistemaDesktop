d9155922a8d6ba7297e1602b3adac16df078cc3a
Beatriz Oliveira <beatrizoliveira863@gmail.com>
2023-04-11 15:18:20 -0300

    Ajustes tela de login

--- LP2_SistemaDesktop/src/br/com/desktop/view/FormLogin.java
+++ LP2_SistemaDesktop/src/br/com/desktop/view/FormLogin.java
@@ -6,10 +6,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class FormLogin extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -52,5 +55,15 @@ public class FormLogin extends JFrame {
 		JButton btnNewButton_1 = new JButton("New button");
 		btnNewButton_1.setBounds(277, 176, 89, 23);
 		panel.add(btnNewButton_1);
+		
+		textField = new JTextField();
+		textField.setBounds(101, 55, 86, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(101, 97, 86, 20);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
 	}
 }
